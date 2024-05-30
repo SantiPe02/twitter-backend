7b5761118fe68fbc61d50f83a046739712611915
santi <santiago.peres@ing.austral.edu.ar>
2024-05-30 09:46:42 -0300

    endpoints to query retweets, likes and comments by userId

--- node/express-prisma-typescript/src/domains/reaction/service/reaction.service.ts
+++ node/express-prisma-typescript/src/domains/reaction/service/reaction.service.ts
@@ -1,4 +1,8 @@
+import { ReactionInputDTO } from '../dto'
+
 export interface ReactionService {
   create: (userId: string, postId: string, reactionType: string) => Promise<void>
   delete: (userId: string, postId: string, reactionType: string) => Promise<void>
+  getLikesByUserId: (userId: string) => Promise<ReactionInputDTO[]>
+  getRetweetsByUserId: (userId: string) => Promise<ReactionInputDTO[]>
 }
